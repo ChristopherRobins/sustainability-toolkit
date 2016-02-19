f954c3aae84176d0e573873baeabf061a4d92bef
serenateng <steng8888@gmail.com>
2016-02-19 12:17:11 -0500

    changed nav bar

--- application/views/header.php
+++ application/views/header.php
@@ -1,7 +1,7 @@
 <body>
   <nav id="topNav">
         <ul>
-          <li class="companies"><a href="http://tavaresgroupconsulting.com" target="_blank"><img src="<?php echo base_url();?>images/tavares-new.png" alt="Tavares Logo"></a></li>
+          
           <li><a href="<?php echo base_url('index.php/home/index')?>">Home</a></li>
           <li class="aboutdropdown"><!-- <a data-dropdown="drop1" aria-controls="drop1" aria-expanded="false">About</a>
             <ul id="drop1" class="f-dropdown" data-dropdown-content aria-hidden="true" tabindex="-1">
@@ -35,6 +35,7 @@
         </li>
           <li><a href="<?php echo base_url('index.php/home/logout')?>">Logout</a></li>
         </ul>
+        <a href="http://tavaresgroupconsulting.com" target="_blank" class="companies"><img src="<?php echo base_url();?>images/tavares-new.png" alt="Tavares Logo"></a>
       </nav>
   <div id="headerWrap" class="large-12 columns">
     <header id="mainHeader" class="row">
